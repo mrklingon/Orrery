796870d9bf7da73968d9783f53e2a9a1af0e865e
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2024-09-06 16:19:09 -0500

    Update orrery.py
    added new solar system

--- orrery.py
+++ orrery.py
@@ -5,19 +5,35 @@ import board
 import touchio
 
 
+def setperiod():
+   return [24, 62, 100, 188]
+
+def rndperiod():
+   return [random.randrange(20)+10,
+        random.randrange(50)+30,
+        random.randrange(80)+30,
+        random.randrange(150)+90]
+
 def setcolor():
     return [(16,16,16), (64,60,7), (0, 62, 0), (36, 0, 0)]
 
 
-period = [24, 62, 100, 188]
+def rndcolor():
+    return [(random.randrange(100),random.randrange(20),random.randrange(20)),
+            (random.randrange(20),random.randrange(20),random.randrange(100)),
+            (random.randrange(20),random.randrange(100),random.randrange(20)),
+            (random.randrange(50),random.randrange(40),random.randrange(30))]
+
+touch1 = touchio.TouchIn(board.TOUCH1)
+touch2 = touchio.TouchIn(board.TOUCH2)
+
+period= setperiod()
+
 planets = ["Mercury", "Venus", "Earth", "Mars"]
 pix = setcolor()
 loc = [0, 0 ,0,0]
 counter = [0,0,0,0]
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=True)
-
-def setcolor():
-    return [(62, 0, 0), (64,60,7), (0, 62, 0), (36, 0, 37)]
 
 def clearplanets(lcx):
     for i in range(4):
@@ -36,6 +52,21 @@ locate = 2 # start at Earth
 
 showplanets(loc)
 while True:
+    Val = 0
+    if touch1.value:
+        Val = Val + 1
+        
+    if touch2.value:
+        Val = Val + 2
+
+    if Val == 2:
+        period = rndperiod()
+        pix = rndcolor()
+
+    if Val == 1:
+        period = setperiod()
+        pix = setcolor()
+
     showplanets(loc)
 
     for p in range(4):
@@ -46,3 +77,6 @@ while True:
             counter[p]=0
 
 #    time.sleep(.01)
+
+
+
